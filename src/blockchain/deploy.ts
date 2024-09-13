cd4f5465c5dae5286e7f5d5dee61d30e5a4952d2
Oxygen033 <moormaxim2k4@gmail.com>
2024-09-13 07:57:21 +0600

    proper .env modififcation

--- src/blockchain/deploy.ts
+++ src/blockchain/deploy.ts
@@ -11,7 +11,19 @@ async function main() {
     const contractAddress = deploymentReceipt.contractAddress;
     console.log("Voting deployed to:", contractAddress);
 
-    fs.writeFileSync('.env', `VOTING_CONTRACT_ADDRESS=${contractAddress}\n`, { flag: 'a' });
+    if (!process.env.VOTING_CONTRACT_ADDRESS) {
+        fs.writeFileSync('.env', `VOTING_CONTRACT_ADDRESS=${contractAddress}\n`, { flag: 'a' });
+    } else {
+        fs.readFile('.env', 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading the file:', err);
+                return;
+            }
+            const updatedContent = data.replace(`VOTING_CONTRACT_ADDRESS=${process.env.VOTING_CONTRACT_ADDRESS}`, `VOTING_CONTRACT_ADDRESS=${contractAddress}\n`);
+
+            fs.writeFileSync('.env', updatedContent, 'utf8');
+        });
+    }
 }
 
 main().catch((error) => {
